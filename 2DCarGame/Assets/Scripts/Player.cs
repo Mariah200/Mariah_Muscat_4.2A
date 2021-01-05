4cfd6c21b66e9ee36679fcdeb7b0304eab8f6ffd
Mariah200 <73109891+Mariah200@users.noreply.github.com>
2021-01-05 19:18:54 +0100

    Commit 3
    Changed movement speed. Arranged errors

--- 2DCarGame/Assets/Scripts/Player.cs
+++ 2DCarGame/Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 //Moving the car to the left and right on the x-axis
 public class Player : MonoBehaviour
-{​​
+{ 
 //These variables can be added  on unity 
     [SerializeField] float moveSpeed = 0.02f;
 
@@ -22,28 +22,28 @@ public class Player : MonoBehaviour
 
     // Start is called 
     void Start()
-    {​​
+    {
         ToSetBoundaries();
-    }​​
+    }
 
 
 
     // Update is called once per frame
     void Update()
-    {​​
+    {
         ToMove();
-    }​​
+    }
 
 
 
     //The car dosen't overlap the camera 
     private void ToSetBoundaries()
-    {​​
+    { 
         Camera gameCamera = Camera.main;
 
 
 
-    //The car dosen't overlap into the x and y axis in position of the camera
+        //The car dosen't overlap into the x and y axis in position of the camera
 
         xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
         xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
@@ -52,19 +52,19 @@ public class Player : MonoBehaviour
 
         yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
-    }​​
+    }
 
 
 
     //To move the car 
     private void ToMove()
-    {​​
+    {
 
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; //for the car to move horizontal
 
 
 
-        var newXPos = transform.position.x + deltaX; 
+        var newXPos = transform.position.x + deltaX;
 
 
 
@@ -74,7 +74,7 @@ public class Player : MonoBehaviour
 
 
         this.transform.position = new Vector2(newXPos, transform.position.y);
-    }​​
-}​
+    }
+}
 
 
