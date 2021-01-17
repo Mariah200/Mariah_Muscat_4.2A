2ba98a25791fa1badaa721b2dcb6e96fa46688b0
Mariah200 <73109891+Mariah200@users.noreply.github.com>
2021-01-17 10:44:13 +0100

    Checked
    arranged some staff

---
+++ 2DCarGame/Assets/Scripts/level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+
+        SceneManager.LoadScene(0); //loads the first scene rather then the game 
+    }
+
+    public void LoadGame()
+    {
+
+        SceneManager.LoadScene("2D Car Game"); //loads the name of the game 
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver"); //When Game is over 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); //to quite 
+    }
+}
+
